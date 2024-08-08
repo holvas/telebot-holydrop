a302d5f4ce6a04b6b083a2d04693e533d71f6773
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-08-08 23:55:37 +0200

    add README & const require

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const cron = require('node-cron');
+const TelegramApi = require('node-telegram-bot-api');
+require('dotenv').config();
+const axios = require('axios');
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramApi(token, {polling: true});
